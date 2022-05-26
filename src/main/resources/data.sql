78ad35eed5d5dc19bf3404821aac087251146cac
Arthur Strokov <arthurstrokov@gmail.com>
2022-05-27 01:42:08 +0300

    up .sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
 insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@gmail.com');
-insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@yandex.ru')
+insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@yandex.ru');
+insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@yandex.by');
+insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@live.ru');
+insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@tut.by');
+insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@yandex.ru');
+insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@ya.ru');
