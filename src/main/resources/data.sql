1792b77b964b1fcfa3384a5df37cd728434af4e0
Arthur Strokov <arthurstrokov@gmail.com>
2022-05-27 00:26:46 +0300

    add .sql for init db in test mode

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@gmail.com');
+insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@yandex.ru')
