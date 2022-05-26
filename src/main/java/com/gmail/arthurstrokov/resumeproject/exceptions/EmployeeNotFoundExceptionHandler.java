4004394a783dcbfb6a08fc5ac2c2b3e3d4c897ff
Arthur Strokov <arthurstrokov@gmail.com>
2022-05-26 21:36:30 +0300

    up EmployeeNotFoundExceptionHandler.java

--- src/main/java/com/gmail/arthurstrokov/resumeproject/exceptions/EmployeeNotFoundExceptionHandler.java
+++ src/main/java/com/gmail/arthurstrokov/resumeproject/exceptions/EmployeeNotFoundExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.gmail.arthurstrokov.resumeproject.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,9 +11,14 @@ import java.util.Collections;
 public class EmployeeNotFoundExceptionHandler {
     private static final String ERROR_MESSAGE = "message";
 
-    @ExceptionHandler({IllegalArgumentException.class, EmployeeNotFoundException.class})
+    @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<?> handle(Exception e) {
         return ResponseEntity.internalServerError()
                 .body(Collections.singletonMap(ERROR_MESSAGE, e.getMessage()));
+    }
+
+    @ExceptionHandler({EmployeeNotFoundException.class})
+    public ResponseEntity<?> handleUserNotFound(Exception exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap(ERROR_MESSAGE, exception.getMessage()));
     }
 }
