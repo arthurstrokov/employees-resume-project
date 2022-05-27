77666c9f20e857eda28c48a5c22637ae5b446906
Arthur Strokov <arthurstrokov@gmail.com>
2022-05-27 11:18:25 +0000

    add javadoc to LoggingAspect.java

--- src/main/java/com/gmail/arthurstrokov/resumeproject/aop/LoggingAspect.java
+++ src/main/java/com/gmail/arthurstrokov/resumeproject/aop/LoggingAspect.java
@@ -8,6 +8,12 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Logging aspect
+ * logging by levels: service, controller, repository
+ *
+ * @author Arthur Strokov
+ */
 @Aspect
 public class LoggingAspect {
     private Logger log;
@@ -17,6 +23,9 @@ public class LoggingAspect {
         log = LoggerFactory.getLogger(loggerName);
     }
 
+    /**
+     * Describe witch packages should be logged.
+     */
     @Pointcut("execution(* com.gmail.arthurstrokov.resumeproject.service.EmployeeService.*(..)) ||" +
             "execution(* com.gmail.arthurstrokov.resumeproject.controller.EmployeeController.*(..)) ||" +
             "execution(* com.gmail.arthurstrokov.resumeproject.repository.EmployeeRepository.*(..))"
@@ -24,6 +33,9 @@ public class LoggingAspect {
     public void executeLogging() {
     }
 
+    /**
+     * Log incoming args
+     */
     @Before("executeLogging()")
     public void beforeAdvice(JoinPoint joinPoint) {
         log = LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringTypeName());
@@ -31,6 +43,9 @@ public class LoggingAspect {
         log.info("Args: {}", joinPoint.getArgs());
     }
 
+    /**
+     * Log returning values
+     */
     @AfterReturning(pointcut = "executeLogging()", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
         log = LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringTypeName());
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,4 @@ insert into employee (first_name, last_name, phone, email) values ('Arthur', 'St
 insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@yandex.by');
 insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@live.ru');
 insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@tut.by');
-insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@yandex.ru');
 insert into employee (first_name, last_name, phone, email) values ('Arthur', 'Strokov', '80291555376', 'arthurstrokov@ya.ru');
